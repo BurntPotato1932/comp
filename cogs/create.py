5eb093856d9897625c6af131c07e49c5ccd920dd
BurntPotato1932 <63624006+BurntPotato1932@users.noreply.github.com>
2023-05-26 22:00:25 -0400

    Update create.py

--- cogs/create.py
+++ cogs/create.py
@@ -5,6 +5,20 @@ from image_process import slide
 import requests
 import discord
 import json
+import shutil
+
+
+def delete_folder(path):
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            file_path = os.path.join(root, name)
+            os.remove(file_path)
+
+        for name in dirs:
+            dir_path = os.path.join(root, name)
+            shutil.rmtree(dir_path)
+
+    shutil.rmtree(path)
 
 def download_image(count, url, folder_path):
     response = requests.get(url)
@@ -15,13 +29,16 @@ def download_image(count, url, folder_path):
         f.close()
     return file_name
 
+class LoopEscapeException(Exception):
+    pass
+
 class Create(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
     @commands.command(name='create_comp', brief='create_comp {name of the comp} {number of cards}')
     @commands.has_any_role(1004278166237487174, 1004976425599766588, 1074003818980847716)
-    async def create_comp(self, ctx, msg: str, rang: int, bg: str):
+    async def create_comp(self, ctx, msg: str, rang: int, bg: str, rng= None):
         dirs = [name for name in os.listdir(".") if os.path.isdir(name)]
         if msg == None:
             await ctx.send('Error no name specified.')
@@ -35,16 +52,14 @@ class Create(commands.Cog):
             data = {
                 "comp": msg,
                 "bg": bg,
-                "inp_path": inp_path,
-                "out_path": out_path
+                "total": rang
             }
+            if rng == "non_rng":
+                data['rng'] = False
             try: 
-                await ctx.send('starting')
+                await ctx.send('Starting!')
                 os.mkdir(inp_path)
                 os.mkdir(out_path)
-                file_path = os.path.join(inp_path, "data.json")
-                with open(file_path, "w") as f:
-                    json.dump(data, f, indent=4)
                 count = 1
                 for i in range(int(rang)):
                     valid_input = False
@@ -58,15 +73,27 @@ class Create(commands.Cog):
                                     url = message.embeds[0].image.url
                                     download_image(count,url, inp_path)
                                     return True
+                                elif message.author.id == ctx.message.author.id and message.content == 'STOP':
+                                    raise LoopEscapeException
                             await self.bot.wait_for('message', check=check)
+                            await ctx.send(f'Added, wating for next card.')
                             valid_input = True
                             count += 1
+                        except LoopEscapeException:
+                            delete_folder(inp_path)
+                            delete_folder(out_path)
+                            await ctx.send('Canceled.')
+                            return 
                         except:
                             await ctx.send("Invalid input, please try again.")
                 await ctx.send(f'Uploaded {count-1} images to .\in_{msg}')
+                file_path = os.path.join(inp_path, "data.json")
+                with open(file_path, "w") as f:
+                    json.dump(data, f, indent=4)
                 slide(inp_path, out_path, bg)
+                await ctx.send(f'Matches made.')
             except OSError as error: 
-                await ctx.send(f'Check console for error')
+                await ctx.send(f'Check console for error.')
                 print(error)
 
 async def setup(bot):
