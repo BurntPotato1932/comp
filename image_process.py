1bf0f7bbcc647b1ab1a5d15d881477324959598f
BurntPotato1932 <63624006+BurntPotato1932@users.noreply.github.com>
2023-05-26 22:04:00 -0400

    Update image_process.py

--- image_process.py
+++ image_process.py
@@ -4,12 +4,21 @@ import random
 import json
 
 def slide(inp, outp, bg):
+    input_folder = inp
+    output_folder = outp
+
     json_file_path = os.path.join("bgs", f"{bg}.json")
     with open(json_file_path, "r") as f:
         data = json.load(f)
-    rn = True
-    input_folder = inp
-    output_folder = outp
+
+    cc_info_json = os.path.join(input_folder, "data.json")
+    with open(cc_info_json, "r") as f:
+        cc_data = json.load(f)
+
+    if "svs" in cc_data:
+        inp2 = cc_data["svs"]
+    else:
+        inp2 = None
 
     def process_image(image1_path, image2_path):
         bg_image = Image.open(data["name"]).convert("RGBA")
@@ -18,12 +27,14 @@ def slide(inp, outp, bg):
 
         bg_width, bg_height = bg_image.size
         output = Image.new("RGBA", (bg_width, bg_height), (0, 0, 0, 0))
-
-        new_size1 = (274, 405)
-        new_image1 = card1.resize(new_size1)
-
-        new_size2 = (274, 405)
-        new_image2 = card2.resize(new_size2)
+        
+        if "2v2" in cc_data:
+            cc_size = (548, 405)
+        else:
+            cc_size = (274, 405)
+    
+        new_image1 = card1.resize(cc_size)
+        new_image2 = card2.resize(cc_size)
 
         x1, y1 = data["x1"], data["y1"]
         x2, y2 = data["x2"], data["y2"]
@@ -34,18 +45,38 @@ def slide(inp, outp, bg):
 
         return output
     
-    if rn == True:
+    if "rng" in cc_data:
+        image_files = sorted(os.listdir(input_folder), key=lambda x: os.path.getmtime(os.path.join(input_folder, x)))
+        if inp2 is not None:
+            image2_files = sorted(os.listdir(inp2), key=lambda x: os.path.getmtime(os.path.join(inp2, x)))
+    else:
         image_files = os.listdir(input_folder)
         random.shuffle(image_files)
-    elif rn == False:
-        image_files = sorted(os.listdir(input_folder), key=lambda x: os.path.getmtime(os.path.join(input_folder, x)))
+        if inp2 is not None:
+            image2_files = os.listdir(inp2)
+            random.shuffle(image2_files)
     image_files = [f for f in image_files if f.endswith(".png")]
+    if inp2 is not None:
+        print(image2_files)
+        image2_files = [f for f in image2_files if f.endswith(".png")]
 
-    if len(image_files) >= 2:
-        for i in range(0, len(image_files)-1, 2):
-            image1_path = os.path.join(input_folder, image_files[i])
-            image2_path = os.path.join(input_folder, image_files[i+1])
+    if inp2 is not None:
+        if len(image_files)+len(image2_files) >= 2:
+            for i in range(len(image_files)):
+                image1_path = os.path.join(input_folder, image_files[i])
+                image2_path = os.path.join(inp2, image2_files[i])
 
-            output_image = process_image(image1_path, image2_path)
-            output_image_path = os.path.join(output_folder, f"{i}_{str(image_files[i][:-4])}-{str(image_files[i+1][:-4])}.png")
-            output_image.save(output_image_path)
+                output_image = process_image(image1_path, image2_path)
+                output_image_path = os.path.join(output_folder, f"{i}_{str(image_files[i][:-4])}-{str(image2_files[i][:-4])}.png")
+                output_image.save(output_image_path)
+    else:
+        im_count = 0
+        if len(image_files) >= 2:
+            for i in range(0, len(image_files)-1, 2):
+                image1_path = os.path.join(input_folder, image_files[i])
+                image2_path = os.path.join(input_folder, image_files[i+1])
+
+                output_image = process_image(image1_path, image2_path)
+                output_image_path = os.path.join(output_folder, f"{im_count}_{str(image_files[i][:-4])}-{str(image_files[i+1][:-4])}.png")
+                output_image.save(output_image_path)
+                im_count += 1
